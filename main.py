005d1a50055c3a54700b9f6ac0ab671d3bc79cac
callum <callumpholt@gmail.com>
2024-01-23 13:38:42 +1100

    added in email capability

--- main.py
+++ main.py
@@ -8,79 +8,103 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-# Function to send message to Telegram channel
+# Function to send email
 
-
-def send_message_to_telegram(token, chat_id, text):
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    payload = {
-        'chat_id': chat_id,
-        'text': text
-    }
-    response = requests.post(url, json=payload)
-    return response.json()
+EMAIL_SENDER = '@gmail.com'
+# Consider using environment variables for security
+EMAIL_PASSWORD = ''
+EMAIL_RECEIVER = '@gmail.com'
 
 
-# Telegram Bot Token and Channel ID
-TOKEN = "6842825878:AAHoVEDTmvZJX3aPJf9U2A1vonX3t02A9LU"
-CHANNEL_ID = -1002118829646
+def send_email(email_sender, email_password, email_receiver, subject, body):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = email_sender
+        msg['To'] = email_receiver
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+        server = smtplib.SMTP('smtp.elasticemail.com', 2525)
+        server.starttls()
+        server.login(email_sender, email_password)
+        text = msg.as_string()
+        server.sendmail(email_sender, email_receiver, text)
+        server.quit()
+        print(f"Email successfully sent to {email_receiver}")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
 
+
+# # Telegram Bot Token and Channel ID
+# TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+# CHANNEL_ID = YOUR_CHANNEL_ID
 # Options for Chrome WebDriver
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument(
-    "user-agent=Mozilla/5.0 (Macintosh; ARM Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.129 Safari/537.36")
+    "user-agent=Mozilla/5.0 (Macintosh; ARM Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.129 Safari/537.36"
+)
 chrome_options.add_argument('--headless')
 
 # Initialize WebDriver
 driver = webdriver.Chrome(options=chrome_options)
 
-# URL input
-url_dynamic = input("Enter the URL for dynamic content: ")
-url_static = input("Enter the URL for static content: ")
+# URLs
+url_dynamic = "https://www.woolworths.com.au/shop/productdetails/118647/friskies-adult-indoor-delights-dry-cat-food"
+url_static = "https://www.coles.com.au/product/chobani-oat-milk-barista-edition-1l-5443825"
 
 while True:
-    # URL for the dynamically loaded content
-    driver.get(url_dynamic)
-    time.sleep(5)  # Wait for content to load
-
-    # Extract the dynamic price
     try:
+        # URL for the dynamically loaded content
+        driver.get(url_dynamic)
+        time.sleep(5)  # Wait for content to load
+
+        # Extract the dynamic price
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "product-panel")))
-        price_element = driver.find_element(By.CLASS_NAME, 'product-panel')
+            EC.presence_of_element_located((By.CLASS_NAME, "price-frame")))
+        price_element = driver.find_element(By.CLASS_NAME, 'price-frame')
         dynamic_content = price_element.text
 
+        # Format the dynamic content to remove new lines and spaces
+        formatted_dynamic_content = re.sub(r'\s+', '', dynamic_content.strip())
+
         # Use regular expression to extract the price per kg
-        match = re.search(r'\$\d+\.?\d* \/ 1KG', dynamic_content)
+        match = re.search(r'\$\d+\.?\d*', formatted_dynamic_content)
         if match:
             price_per_kg = match.group()
-            print("Price per KG:", price_per_kg)
+            print("Price of Woolworths Item:", price_per_kg)
         else:
-            print("Price per KG not found")
+            print("Price of Woolworths Item not found")
+
     except Exception as e:
         print(f"Error extracting content: {e}")
 
-    # URL for the static content
-    response = requests.get(url_static)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    script = soup.find('script', type='application/ld+json')
-
-    # Extract the static price
     try:
+        # URL for the static content
+        response = requests.get(url_static)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script = soup.find('script', type='application/ld+json')
+
+        # Extract the static price
         data = json.loads(script.string)
         static_price = data['offers'][0]['price']
-        print("Coles Price: $", static_price)
+
+        print(f"Price of Coles Item: ${static_price}")
+
+        # Prepare message
+        message = f"Price per KG: {price_per_kg}\nColes Price: ${static_price}"
+
+        # Send message to Telegram channel
+        subject = "Price Update"
+        send_email(EMAIL_SENDER, EMAIL_PASSWORD,
+                   EMAIL_RECEIVER, subject, message)
+
     except Exception as e:
         print("Error extracting static price:", e)
-
-    # Prepare message
-    message = f"Price per KG: {price_per_kg}\nColes Price: ${static_price}"
-
-    # Send message to Telegram channel
-    send_message_to_telegram(TOKEN, CHANNEL_ID, message)
 
     # Wait for 60 seconds before the next iteration
     time.sleep(60)
